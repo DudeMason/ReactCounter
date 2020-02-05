e9c93d197c9634712d0dd8a254a9678e77d43acc
Mason Eyre <mason.deyre@gmail.com>
2020-02-04 19:55:54 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,34 +3,34 @@ import './App.css';
 
 function App() {
 
-  const [session, countSession] = useState(0)
-  const [total, countTotal] = useState(0)
-  const [prev, countPrev] = useState(0)
-  const [prevTote, countPrevTote] = useState(0)
+  const [session, sessionCount] = useState(0)
+  const [total, totalCount] = useState(0)
+  const [previousSession, previousSessionCount] = useState(0)
+  const [previousTotal, previousTotalCount] = useState(0)
 
   return (
     <div>
       <div className='session'>
-        Previous Session: {prev}
+        Previous Session: {previousSession}
         <br/>
         Current Session: {session}
       </div>
       <div className='total'>
-        Previous Total: {prevTote}
+        Previous Total: {previousTotal}
       </div>
       <br/>
-      <button className='counter' onClick={() => {countSession(session + 1); countTotal(total + 1);}}>
+      <button className='counter' onClick={() => {sessionCount(session + 1); totalCount(total + 1);}}>
         Total:
         <br/>
         {total}
       </button>
       <br/>
-      <button className='reset' onClick={() => {countPrev(session); countSession(0); countTotal(0);}}>
+      <button className='reset' onClick={() => {previousSessionCount(session); sessionCount(0); totalCount(0);}}>
         Clear
         <br/>
         Session
       </button>
-      <button className='clear' onClick={() => {countPrevTote(); countTotal(0);}}>
+      <button className='clear' onClick={() => {previousTotalCount(total); totalCount(0);}}>
         Clear
         <br/>
         Total
